4eb6943fb8cbe9dcf071c206b1b6f4aceb951545
Landia Yung <landia.yung@schoolteam.com.hk>
2022-06-17 23:12:53 +0800

    updates for the specific api

--- index.js
+++ index.js
@@ -7,40 +7,18 @@ const app = express()
 
 app.use(cors())
 
-app.get('/word', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-        params: {count: '5', wordLength: '5'},
-        headers: {
-            'x-rapidapi-host': 'random-words5.p.rapidapi.com',
-            'x-rapidapi-key': process.env.RAPID_API_KEY
-        }
-    }
-    axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data[0])
-    }).catch((error) => {
-        console.error(error)
-    })
-})
-
+app.use(express.static('static'))
 
 app.get('/check', (req, res) => {
     const word = req.query.word
+    const seed = req.query.seed
 
     const options = {
         method: 'GET',
-        url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
-        params: {entry: word},
-        headers: {
-            'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
-            'x-rapidapi-key': process.env.RAPID_API_KEY
-        }
+        url: `https://v1.wordle.k2bd.dev/random?guess=${word}&seed=${seed}`,
     }
     axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data.result_msg)
+        res.json(response.data)
     }).catch((error) => {
         console.error(error)
     })
